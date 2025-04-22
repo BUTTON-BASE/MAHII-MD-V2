8fdb408038a559c7c0fc08a431399898cad8a933
Mahii-Botz <herokutest31@gmail.com>
2025-04-22 09:16:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = "true") {
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "yZRG2QIC#dDWrpypLQXIefoz5V92nE4cS1baKLBeC6Xr1v0hX3nU",
   MONGODB: process.env.MONGODB || "mongodb://mongo:BkspBConSbfMmxoPWhCAtizcPcVFEnIb@hopper.proxy.rlwy.net:36316",
-  OWNER_NUM: process.env.OWNER_NUM || "94715450089",
+  OWNER_NUM: process.env.OWNER_NUM || "94721229780",
 };
