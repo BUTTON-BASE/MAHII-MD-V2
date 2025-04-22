1b1a388b2755b06f6f9ac67f5df4e0d2df08dc78
Mahii-Botz <herokutest31@gmail.com>
2025-04-22 08:51:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
+  SESSION_ID: process.env.SESSION_ID || "yZRG2QIC#dDWrpypLQXIefoz5V92nE4cS1baKLBeC6Xr1v0hX3nU",
   MONGODB: process.env.MONGODB || "mongodb://mongo:BkspBConSbfMmxoPWhCAtizcPcVFEnIb@hopper.proxy.rlwy.net:36316",
   OWNER_NUM: process.env.OWNER_NUM || "94715450089",
 };
