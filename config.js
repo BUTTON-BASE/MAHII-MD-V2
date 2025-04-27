38383cebd7e8ac90c776bcbe135f1f5d48c0d1ad
Mahii-Botz <herokutest31@gmail.com>
2025-04-27 10:30:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = "true") {
 }
 
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "SIpziSAD#x_tKISWZYPfi_Er8lLj1VXepRsKJ2GofT5XP4Tr9jZQ",
+  SESSION_ID: process.env.SESSION_ID || "OYwBiKiD#8wddEMw8WFTBYH-YixxsKk-ScQQt953bfoUcA3Yo5Hk",
   OWNER_NUM: process.env.OWNER_NUM || "94714971189",
   PREFIX: process.env.PREFIX || ".",
   ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Mahii-Botz/Mahii-md-LOGO/blob/main/ChatGPT%20Image%20Apr%2021,%202025,%2005_32_50%20PM.png?raw=true",
