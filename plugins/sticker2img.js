aaed5873ae51860e75a25b14c58f45a830a3193d
Mahii-Botz <herokutest31@gmail.com>
2025-04-27 20:30:10 +0530

    Update sticker2img.js

--- plugins/sticker2img.js
+++ plugins/sticker2img.js
@@ -53,8 +53,8 @@ cmd(
 
       // Convert the sticker buffer to an image (using Sticker class)
       const sticker = new Sticker(stickerBuffer, {
-        pack: "𝐑_𝐎_𝐁_𝐈_𝐍",
-        author: "𝐒_𝐈_𝐇_𝐈_𝐋_𝐄_𝐋",
+        pack: "𝗠𝗔𝗛𝗜𝗜-𝗠𝗗",
+        author: "𝗠𝗛𝗜𝗥𝗔𝗡𝗚𝗔",
         type: "FULL", // This may not be needed, but ensures we're using the full sticker format
         quality: 100, // Quality of the output image (0-100)
       });
@@ -67,7 +67,7 @@ cmd(
         from,
         {
           image: imageBuffer,
-          caption: "Here is your converted image!\n\n𝐌𝐚𝐝𝐞 𝐛𝐲 𝐒_𝐈_𝐇_𝐈_𝐋_𝐄_𝐋",
+          caption: "Here is your converted image!\n\n 𝗠𝗔𝗗𝗘 𝗕𝗬 𝗠𝗛𝗜𝗥𝗔𝗡𝗚𝗔",
         },
         { quoted: mek }
       );
