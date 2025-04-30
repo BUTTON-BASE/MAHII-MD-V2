7bbd6a5e3d28363746227ad422499763d71aea57
AmilaPrabathKumara <Amilaprabath0762280384v2@gmail.com>
2025-04-30 21:49:33 +0530

    Update status_save.js

--- plugins/status_save.js
+++ plugins/status_save.js
@@ -1,105 +1,67 @@
 const { cmd } = require("../command");
-const { getRandom } = require('../lib/functions');
-const fs = require('fs');
 
 cmd(
   {
-    on: "body"
+    on: "body",
   },
-  async (robin, mek, m, { from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    if (!m.quoted) return;
-    if (!mek && !mek.message && !body) return;
+  async (
+    robin,
+    mek,
+    m,
+    {
+      body,
+      reply,
+    }
+  ) => {
+    if (!m.quoted || !body) return;
 
-const data = JSON.stringify(mek.message, null, 2)
-const jsonData = JSON.parse(data);
-const isStatus = jsonData?.extendedTextMessage?.contextInfo?.remoteJid;
+    const bdy = body.toLowerCase();
+    const keywords = [
+      "දියම්", "දෙන්න", "දාන්න", "එවන්න", "ඕන", "ඕනා", "එවපන්", "දාපන්", "එව්පන්",
+      "send", "give", "ewpn", "ewapan", "ewanna", "danna", "dpn", "dapan", "ona",
+      "daham", "diym", "dhm", "save", "status", "ඕනි", "ඕනී", "ewm", "ewnn"
+    ];
 
-if(!isStatus) return;
-let bdy = body.toLowerCase();
-let keywords = ["දියම්", "දෙන්න", "දාන්න", "එවන්න", "ඕන", "ඕනා", "එවපන්", "දාපන්", "එව්පන්", "send", "give", "ewpn", "ewapan", "ewanna","danna", "ewapan", "dpn", "dapan", "ona","daham", "diym", "dhm", "save","status","ඕනි","ඕනී","ewm","ewnn"];  // මෙතනට උබට තව වචන ඕන්නම් ඔය විදිහට ඇඩ් කරගන්න 👈
-let kk = keywords.map(word => word.toLowerCase());
-	  
-if (kk.includes(bdy)) {
-if(m.quoted.type === 'imageMessage') {
- var nameJpg = getRandom('');
- let buff =  await m.quoted.download(nameJpg)
- let fileType = require('file-type');
- let type = await fileType.fromBuffer(buff);
- await fs.promises.writeFile("./" + type.ext, buff);
+    if (!keywords.includes(bdy)) return;
 
+    const quoted = m.quoted;
+    const buffer = await quoted.download();
 
-const mimetype = m.quoted.imageMessage.mimetype;
-const caption = m.quoted.imageMessage.caption;
-const inputString = m.quoted.sender
-const splitArray = inputString.split("@");
-const phoneNumber = splitArray[0].replace(/\D/g, ''); 
+    switch (quoted.type) {
+      case "imageMessage":
+        return await robin.sendMessage(m.chat, { image: buffer, caption: quoted.caption || "" }, { quoted: mek });
 
+      case "videoMessage":
+        return await robin.sendMessage(
+          m.chat,
+          {
+            video: buffer,
+            mimetype: "video/mp4",
+            fileName: `${m.id}.mp4`,
+            caption: quoted.caption || "",
+          },
+          { quoted: mek }
+        );
 
+      case "audioMessage":
+        return await robin.sendMessage(
+          m.chat,
+          {
+            audio: buffer,
+            mimetype: "audio/mp4",
+            ptt: true,
+          },
+          { quoted: mek }
+        );
 
-	
-const cap = `${caption}`;	
+      case "stickerMessage":
+        return await robin.sendMessage(m.chat, { sticker: buffer }, { quoted: mek });
 
+      case "extendedTextMessage":
+        return await robin.sendMessage(m.chat, { text: quoted.text || "❌ Couldn't get message" }, { quoted: mek });
 
-return await robin.sendMessage(from, { image: fs.readFileSync("./" + type.ext), caption: cap })
-
-}else if(m.quoted.type === 'videoMessage') {
-var nameJpg = getRandom('');
- let buff =  await m.quoted.download(nameJpg)
- let fileType = require('file-type');
- let type = await fileType.fromBuffer(buff);
- await fs.promises.writeFile("./" + type.ext, buff);	
-
-
-const mimetype = m.quoted.videoMessage.mimetype;
-const seconds = m.quoted.videoMessage.seconds;
-const caption = m.quoted.videoMessage.caption;
-const inputString = m.quoted.sender
-const splitArray = inputString.split("@");
-const phoneNumber = splitArray[0].replace(/\D/g, ''); 
-
-
-
-	
-const cap = `${caption}`;	
-let buttonMessage = {
-              video: fs.readFileSync("./" + type.ext),
-              mimetype: "video/mp4",
-              fileName: `${m.id}.mp4`,
-              caption: cap,
-              headerType: 4
-            };
-           return await robin.sendMessage(from, buttonMessage, {
-              quoted: mek
-            });
-}else if(m.quoted.type === 'audioMessage') {
-var nameJpg = getRandom('');
- let buff =  await m.quoted.download(nameJpg)
- let fileType = require('file-type');
- let type = await fileType.fromBuffer(buff);
- await fs.promises.writeFile("./" + type.ext, buff);	
-let buttonMessage = {
-              audio: fs.readFileSync("./" + type.ext),
-              mimetype: "audio/mp3",
-              fileName: `${m.id}.mp3`,
-              caption: `✉️ *audioMessage*`,
-              headerType: 4
-            };
-           return await robin.sendMessage(from, buttonMessage, {
-              quoted: mek
-            });
-
-}else if(m.quoted.type === 'stickerMessage') {
-var nameJpg = getRandom('');
- let buff =  await m.quoted.download(nameJpg)
- let fileType = require('file-type');
- let type = await fileType.fromBuffer(buff);
- await fs.promises.writeFile("./" + type.ext, buff);	
-
-return await robin.sendMessage(from,{sticker: fs.readFileSync("./" + type.ext),package: 'sticker'},{ quoted: mek })
-
-}else if(m.quoted.type === 'extendedTextMessage') {
-await robin.sendMessage(from,{text: m.quoted.msg.text })
-	  	
-}    
-}
-})
+      default:
+        return reply("😥 *invalid media type.*");
+    }
+  }
+);
