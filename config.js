9d20ee1a0c2d8b2119e1d8f440c144b95148f75e
Mahii-Botz <herokutest31@gmail.com>
2025-05-25 16:12:20 +0530

    𝑴𝑨𝑯𝑰𝑰-𝑴𝑫

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = "true") {
 }
 
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "g7swyJBC#Zt0ZGARXh4MxrFvm7ooHbwhkl6-3ZhyH_EZ_go44a8U",
+  SESSION_ID: process.env.SESSION_ID || "JvVymAhL#fH5sJkcUNyzDwKv4g7KNJ_unxxPu-GgGiHBdIFBxTgQ",
   OWNER_NUM: process.env.OWNER_NUM || "94721229780",
   PREFIX: process.env.PREFIX || ".",
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
